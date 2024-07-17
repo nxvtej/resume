9312b50d321ba34e16dc9043b812e9ce6871fec2
nxvtej <nxvtej@outlook.com>
2024-07-17 16:22:30 +0530

    add create route"

--- ai-resume-builder/src/dashboard/index.jsx
+++ ai-resume-builder/src/dashboard/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import AddResume from "./component/AddResume";
 
 const DashboardPage = () => {
-	return <div>DashboardPage</div>;
+	return (
+		<div className='p-10 md:px-20lg:px-32'>
+			<h2 className='font-bold text-3xl'>My resume</h2>
+			<p>Start creating resume for your next job</p>
+			<div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-10'>
+				<AddResume />
+			</div>
+		</div>
+	);
 };
 
 export default DashboardPage;
